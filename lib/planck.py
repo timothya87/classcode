67e8879e9d91388ded77e4bfb86a0317ead5b56c
Phil Austin <paustin@eos.ubc.ca>
2014-10-26 19:59:00 -0700

    new test code

--- lib/planck.py
+++ lib/planck.py
@@ -3,11 +3,13 @@
 """
 
 import numpy as np
+from  scipy.integrate import quad
 
 h=6.63e-34  #planck's constant (J s)
 kb=1.38e-23 # Boltzman's constant (J K^{-1})
 c=3.e8  #speed of light (m/s)
 c2=h*c/kb
+sigma=2.*np.pi**5.*kb**4./(15*h**3.*c**2.)
 
 def WHplanck(wavel,Temp):
     """
@@ -70,13 +72,15 @@ def planckInvert(wavel,Blambda):
     Tbright=c2/(wavel*(np.log(c1) -np.log(np.pi*wavel**5.) - np.log(Blambda)))
     return Tbright
 
-def planckInt(wavel,Temp):
-    print "inside planckInt"
-    dlamb=wavel[1:] - wavel[:-1]
-    bbr=planckwavelen(wavel[:-1],Temp)
-    integ=np.sum(bbr*dlamb)
-    return integ
+def planckInt(Temp,lower,upper):
+    """Integrate planckwavelen given temperatue Temp (K) from lower (m) to upper (m) wavelengths
 
+       output: integrated radiance in W/m^2/sr
+       see http://docs.scipy.org/doc/scipy-0.14.0/reference/integrate.html#module-scipy.integrate
+    """
+    args=(Temp)
+    integ=quad(planckwavelen,lower,upper,args)
+    return integ[0]
 
 
 def goodInvert(T0,bbr,wavel):
@@ -123,7 +127,43 @@ def test_planck_wavelen():
             out.append(the_bbr)
     answer=[0.4498, 0.8921, 1.1922, 2.7226, 3.7736, 3.9804, 21.4025, 19.8225, 16.0759]
     np.testing.assert_array_almost_equal(out,answer,decimal=4)
-    return out
+    return None
+
+def test_planck_inverse():
+    """
+       test planck inverse for several round trips
+       and Temps
+    """
+    #
+    # need Temp in K and wavelen in m
+    #
+    the_temps=[200., 250., 350.]
+    the_wavelens=np.array([8.,10.,12.])*1.e-6
+    out=[]
+    for a_temp in the_temps:
+        for a_wavelen in the_wavelens:
+            #
+            # convert to W/m^2/micron/sr
+            #
+            the_bbr=planckwavelen(a_wavelen,a_temp)
+            out.append((a_wavelen,the_bbr))
+
+    brights=[]
+    for wavelen,bbr in out:
+        brights.append(planckInvert(wavelen,bbr))
+    answer=[200.0, 200.0, 200.0, 250.0, 250.0, 250.0, 350.0, 350.0, 350.0]
+    np.testing.assert_array_almost_equal(brights,answer,decimal=10)
+    return None
+
+def test_planck_integral():
+    """
+       integrage and compare with stefan-boltzman
+    """
+    Temp=300.
+    stefan=sigma/np.pi*Temp**4.
+    totrad=planckInt(Temp,1.e-7,8000.e-6)
+    np.testing.assert_almost_equal(totrad,stefan,decimal=5)
+    return None
 
 #this trick will run  the following script if
 #the file planck.py is run as a program, but won't
@@ -133,65 +173,5 @@ def test_planck_wavelen():
 if __name__ ==  '__main__':
 
     test_planck_wavelen()
-
-##     Temp=262.
-##     #check to see how good this approx. is:
-
-##     wavel=11.e-6
-
-##     bbr= planckwavelen(wavel,Temp)
-##     bt = planckInvert(wavel,bbr)
-
-##     print "pa: Temp (K), bbr (W/m^2/sr/m), bt (K): ",Temp,bbr*1.e-6,bt
-
-##     wavel=np.arange(1.,2000.,.01)*1.e-6
-##     print "size of  wavel: ",wavel.shape
-
-##     bbr=planckwavelen(wavel,Temp)
-##     print "size of  bbr: ",bbr.shape
-##     print "type of bbr:", type(bbr)
-
-##     totrad=planckInt(wavel,Temp)
-##     stefan=5.67e-8*Temp**4.
-##     print "approx integ: ",totrad," stefan-boltzman: ",stefan/np.pi
-
-##     wavel=wavel*1.e6
-##     totrad=planckInt(wavel,Temp)
-##     stefan=5.67e-8*Temp**4.
-##     print "approx Micron integ: ",totrad," stefan-boltzman: ",stefan/np.pi
-
-    
-## #check the derivitive
-
-##     Temp=np.arange(284.,286.,0.1)
-##     wavel=10.e-6
-##     bbr= planckwavelen(wavel,Temp)
-##     dbbrdT=(bbr[1:]-bbr[0:-1])/(Temp[1:]-Temp[0:-1])
-
-##     Temp=285.
-##     theDeriv=planckDeriv(wavel,Temp)
-##     print "one deriv: ",dbbrdT[len(dbbrdT)/2]
-##     print "two deriv: ",theDeriv
-
-
-##     wavel=10.e-6
-##     T0=320.
-##     bbr=planckwavelen(wavel,300.)
-##     guess=goodInvert(T0,bbr,wavel)
-##     print "approx root with T0=299: ",guess
-
-##     wavel=10.e-6
-##     T0=320.
-##     bbr=np.array([planckwavelen(wavel,300.),planckwavelen(wavel,310.),
-##                   planckwavelen(wavel,290.)])
-##     guess=rootfind(T0,bbr,wavel)
-##     print "rootfind root with T0=320: ",guess
-
-##     wavenum=np.arange(25,1600,20) #in inverse cm
-##     c=3.e8
-##     c_cm=c*100. #convert speed of light to cm/s
-##     freq=c_cm*wavenum
-##     Temp=280.
-##     h=6.63e-34
-##     kb=1.38e-23
-##     print planckfreq(freq,Temp)
+    test_planck_inverse()
+    test_planck_integral()

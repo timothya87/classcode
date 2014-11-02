b70625f85fa3fb994e806190aebda6e3c2e0e620
Phil Austin <paustin@eos.ubc.ca>
2014-11-02 15:15:37 -0800

    readcloudsat.py and draw_orbit.py working with hdf5

--- lib/cloudsat/readcloudsat.py
+++ lib/cloudsat/readcloudsat.py
@@ -5,13 +5,14 @@ import dateutil.tz as tz
 import numpy as np
 import matplotlib.pyplot as plt
 import h5py
-from netCDF4 import Dataset
-import pyhdf.SD
-from pyhdf import VS
-import pyhdf
-import pyhdf.HDF as HDF
+
 
 def convert_field(void_field):
+    """
+      convert a numpy array of tuples
+      into a regular numpy array of the same
+      shape and dtype
+    """
     save_shape=void_field.shape
     flat_test=void_field.flat
     out_flat=np.empty(len(flat_test),dtype=flat_test[0][0].dtype)
@@ -54,92 +55,80 @@ def get_geo(hdfname):
     #now loop throught he radar profile times and convert them to 
     #python datetime objects in utc
     for the_time in var_dict['Profile_time']:
-        time_vals.append(orbitStart + datetime.timedelta(seconds=float(the_time)))
-    var_dict['time_vals']=time_vals
+        date_time=orbitStart + datetime.timedelta(seconds=float(the_time))
+        time_vals.append(date_time)
+    var_dict['date_day']=time_vals
     neg_values=var_dict['DEM_elevation'] < 0
     var_dict['DEM_elevation'][neg_values]=0
-    return var_dict
+    #
+    # return a list with the five variables
+    #
+    variable_names=['Latitude','Longitude','date_day','Profile_time','DEM_elevation']
+    out_list=[var_dict[varname] for varname in variable_names]
+    return out_list
 
 if __name__=="__main__":
     #this flag makes sure the data file can't be overwritten
     #radar reflectivity data see
     #http://www.cloudsat.cira.colostate.edu/dataSpecs.php?prodid=9
     radar_file='2010247105814_23156_CS_2B-GEOPROF_GRANULE_P_R04_E03.h5'
-    geo_vars_radar=get_geo(radar_file)
+    lat,lon,date_day,prof_seconds,dem_elevation=get_geo(radar_file)
     lidar_file='2010247105814_23156_CS_2B-GEOPROF-LIDAR_GRANULE_P2_R04_E03.h5'
-    geo_vars_lidar=get_geo(lidar_file)
-    radar_file='2009197050922_17109_CS_2C-RAIN-PROFILE_GRANULE_P_R04_E02.h5'
-    geo_vars_radar=get_geo(radar_file)
-    test=Dataset('2009197050922_17109_CS_2C-RAIN-PROFILE_GRANULE_P_R04_E02.nc')
-    hdfname='2009197050922_17109_CS_2C-RAIN-PROFILE_GRANULE_P_R04_E02.hdf'
-    hdffile=HDF.HDF(hdfname,HDF.HC.READ)
-    var_name='Latitude'
-    vs=hdffile.vstart()
-    the_var=vs.attach(var_name)
-    nrecs=the_var._nrecs
-    the_data=the_var.read(nRec=nrecs)
-    the_data=np.array(the_data).squeeze()
-    var_name='Longitude'
-    vs=hdffile.vstart()
-    the_var=vs.attach(var_name)
-    nrecs=the_var._nrecs
-    the_data=the_var.read(nRec=nrecs)
-    the_data=np.array(the_data).squeeze()
-
     ## #
     ## # height values stored as an SD dataset
     ## #
-    ## hdf_SD=pyhdf.SD.SD(radar_file,readonly)
-    ## height_sd=hdf_SD.select('Height')
-    ## height_vals=height_sd.get()
-    ## height=height_vals.astype(np.float)
-    ## refl=hdf_SD.select('Radar_Reflectivity')
-    ## refl_vals=refl.get()
-    ## scale_factor=refl.attributes()['factor']
-    ## hdf_SD.end()
-    ## #print refl.attributes()
-    ## refl_vals=refl_vals/scale_factor
-    ## lidar_file='2008291181813_13156_CS_2B-GEOPROF-LIDAR_GRANULE_P1_R04_E02.hdf'
-    ## lidar_file='2010247105814_23156_CS_2B-GEOPROF-LIDAR_GRANULE_P2_R04_E03.hdf'
-    ## lidar_data=pyhdf.SD.SD(lidar_file,readonly)
-    ## layerTop=lidar_data.select('LayerTop')
-    ## layerTop=layerTop.get()
-    ## layerTop=layerTop.astype(np.float)
-    ## layerTop[layerTop < 0]=np.nan
+    with h5py.File(radar_file,'r') as f:
+        height=f['2B-GEOPROF']['Geolocation Fields']['Height'].value
+        height=height.astype(np.float)
+        refl_vals=f['2B-GEOPROF']['Data Fields']['Radar_Reflectivity'].value
+        refl_vals=refl_vals.astype(np.float)
+        refl_scale=(f['2B-GEOPROF']['Swath Attributes']['Radar_Reflectivity.factor'].value)[0][0]
+        refl_vals=refl_vals/refl_scale
+    with h5py.File(lidar_file,'r') as f:
+        layerTop=f['2B-GEOPROF-LIDAR/Data Fields/LayerTop'].value
+        layerTop=layerTop.astype(np.float)
+        layerTop[layerTop < 0]=np.nan
+
+    plt.close('all')
+    fig1=plt.figure(1)
+    fig1.clf()
+    axis1=fig1.add_subplot(1,1,1)
+    start=21000
+    stop=22000
+    start=5000
+    stop=6000
+    #
+    # subset the array
+    #
+    part_refl=refl_vals[start:stop,:]
+    #
+    # mask out the uninteresting reflectivities
+    #
+    hit=np.logical_or(part_refl < -5.,part_refl > 20)
+    refl_masked=np.ma.masked_array(part_refl,hit)
+    #
+    # convert height to km
+    #
+    im=axis1.pcolormesh(prof_seconds[start:stop],height[0,:]/1.e3,refl_masked.T)
+    axis1.set_xlabel('time after orbit start (seconds)')
+    axis1.set_ylabel('height (km)')
+    start,stop=[item.strftime('%Y-%m-%d %H:%M:%S') for item in (date_day[start],date_day[stop])]
+    axis1.set_title('{} to {}'.format(start,stop))
+    axis1.set_ylim([0,10])
+    cb=fig1.colorbar(im)
+    cb.set_label('reflectivity (dbZ)')
+    fig1.savefig('reflectivity.png')
+
     
-    ## fig1=plt.figure(1)
-    ## fig1.clf()
-    ## axis1=fig1.add_subplot(1,1,1)
-    ## start=21000
-    ## stop=22000
-    ## start=5000
-    ## stop=6000
-    ## #
-    ## # subset the array
-    ## #
-    ## part_refl=refl_vals[start:stop,:]
-    ## #
-    ## # mask out the uninteresting reflectivities
-    ## #
-    ## hit=np.logical_or(part_refl < -5.,part_refl > 20)
-    ## refl_masked=np.ma.masked_array(part_refl,hit)
-    ## im=axis1.pcolor(prof_seconds[start:stop],height[0,:]/1.e3,refl_masked.T)
-    ## axis1.set_xlabel('time after orbit start (seconds)')
-    ## axis1.set_ylabel('height (km)')
-    ## axis1.set_ylim([0,10])
-    ## cb=fig1.colorbar(im)
-    ## cb.set_label('reflectivity (dbZ)')
-    ## fig1.savefig('reflectivity.png')
-    
-    ## fig2=plt.figure(2)
-    ## axis2=fig2.add_subplot(1,1,1)
-    ## axis2.plot(prof_seconds,layerTop[:,0]/1.e3,'b')
-    ## axis2.plot(prof_seconds,dem_elevation/1.e3,'r')
-    ## axis2.set_xlabel('time after orbit start (seconds)')
-    ## axis2.set_ylabel('height (km)')
-    ## axis2.set_title('lidar cloud top (blue) and dem surface elevation (red)')
-    ## fig2.savefig('lidar_height.png')
-    ## plt.show()
+    fig2=plt.figure(2)
+    axis2=fig2.add_subplot(1,1,1)
+    axis2.plot(prof_seconds,layerTop[:,0]/1.e3,'b')
+    axis2.plot(prof_seconds,dem_elevation/1.e3,'r')
+    axis2.set_xlabel('time after orbit start (seconds)')
+    axis2.set_ylabel('height (km)')
+    axis2.set_title('lidar cloud top (blue) and dem surface elevation (red)')
+    fig2.savefig('lidar_height.png')
+    plt.show()
 
     
 
--- lib/cloudsat/readrain.py
+++ lib/cloudsat/readrain.py
@@ -5,61 +5,56 @@ import dateutil.tz as tz
 import numpy as np
 import matplotlib.dates as mpldates 
 import matplotlib.pyplot as plt
-import pyhdf.SD
-from pyhdf import VS
-import pyhdf
-import pyhdf.HDF as HDF
+import h5py
 import sys
-from readcloudsat import get_geo
+from readcloudsat import get_geo,convert_field
 
 if __name__=="__main__":
-    #this flag makes sure the data file can't be overwritten
-    readonly=pyhdf.SD.SDC.READ
     #http://www.cloudsat.cira.colostate.edu/dataSpecs.php?prodid=9
-    rainfilename='2008291181813_13156_CS_2C-RAIN-PROFILE_GRANULE_P_R04_E02.hdf'
-    rainfilename='2009197050922_17109_CS_2C-RAIN-PROFILE_GRANULE_P_R04_E02.hdf'
+    rainfilename='2010247105814_23156_CS_2C-RAIN-PROFILE_GRANULE_P_R04_E03.h5'
+    rainfilename='2009197050922_17109_CS_2C-RAIN-PROFILE_GRANULE_P_R04_E02.h5'
     lat,lon,time_utc,prof_seconds,dem_elevation=get_geo(rainfilename)
-    hdffile=HDF.HDF(rainfilename,HDF.HC.READ)
-    vs=hdffile.vstart()
-    rain_rate_vs=vs.attach('rain_rate')
-    nrecs=rain_rate_vs._nrecs
-    rain_rate=rain_rate_vs.read(nRec=nrecs)
-    rain_rate=np.array(rain_rate)
-    rain_rate[rain_rate < 0]=np.nan
-        
-    fig1=plt.figure(1)
-    fig1.clf()
-    axis1=fig1.add_subplot(1,1,1)
-    axis1.plot(rain_rate)
-    axis1.set_xlabel('index value')
-    axis1.set_ylabel('rain rate (mm/hour)')
-    axis1.set_title('rain rate for all values')
-    fig1.savefig('rain1.png')
+    with h5py.File(rainfilename,'r') as f:
+        rain_rate=f['2C-RAIN-PROFILE']['Data Fields']['rain_rate'].value
+        rain_rate=[item[0] for item in rain_rate]
+        rain_rate=np.array(rain_rate)
+    #rain_rate[rain_rate < -9000]=np.nan
+    plt.close('all')
+    plt.hist(rain_rate)
+    plt.show()    
+    ## fig1=plt.figure(1)
+    ## fig1.clf()
+    ## axis1=fig1.add_subplot(1,1,1)
+    ## axis1.plot(rain_rate)
+    ## axis1.set_xlabel('index value')
+    ## axis1.set_ylabel('rain rate (mm/hour)')
+    ## axis1.set_title('rain rate for all values')
+    ## fig1.savefig('rain1.png')
     
-    fig2=plt.figure(2)
-    fig2.clf()
-    axis2=fig2.add_subplot(1,1,1)
-    start=20000
-    stop=22000
-    axis2.plot(rain_rate[start:stop])
-    axis2.set_xlabel('index value')
-    axis2.set_ylabel('rain rate (mm/hour)')
-    axis2.set_title('rain rate for 2000 values')
-    fig2.savefig('rain2.png')
+    ## fig2=plt.figure(2)
+    ## fig2.clf()
+    ## axis2=fig2.add_subplot(1,1,1)
+    ## start=20000
+    ## stop=22000
+    ## axis2.plot(rain_rate[start:stop])
+    ## axis2.set_xlabel('index value')
+    ## axis2.set_ylabel('rain rate (mm/hour)')
+    ## axis2.set_title('rain rate for 2000 values')
+    ## fig2.savefig('rain2.png')
 
-    #
-    # use matplotlib's date formatter to format the xaxis in
-    # mm:ss utc
-    #
-    fig3=plt.figure(3)
-    fig3.clf()
-    axis3=fig3.add_subplot(1,1,1)
-    formatter = mpldates.DateFormatter('%H:%M')
-    axis3.xaxis.set_major_formatter(formatter)
-    axis3.plot(time_utc[start:stop],rain_rate[start:stop])
-    axis3.set_xlabel('time (UTC)')
-    axis3.set_ylabel('rain rate (mm/hour)')
-    fig3.savefig('rain3.png')
-    plt.show()
+    ## #
+    ## # use matplotlib's date formatter to format the xaxis in
+    ## # mm:ss utc
+    ## #
+    ## fig3=plt.figure(3)
+    ## fig3.clf()
+    ## axis3=fig3.add_subplot(1,1,1)
+    ## formatter = mpldates.DateFormatter('%H:%M')
+    ## axis3.xaxis.set_major_formatter(formatter)
+    ## axis3.plot(time_utc[start:stop],rain_rate[start:stop])
+    ## axis3.set_xlabel('time (UTC)')
+    ## axis3.set_ylabel('rain rate (mm/hour)')
+    ## fig3.savefig('rain3.png')
+    ## plt.show()
 
      
